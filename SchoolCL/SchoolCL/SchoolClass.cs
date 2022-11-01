e1a748def374e03d88752186efce1d2080aa34c9
Nandhinipriya <nandhinipriya745@gmail.com>
2022-11-01 22:03:53 +0530

    Add project files.

---
+++ SchoolCL/SchoolCL/SchoolClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCL
+{
+    public class SchoolClass
+    {
+        public string Classname { get; set; }
+        public int Noofstudents { get; set; }
+    }
+    public class Marks
+    {
+        public int marks { get; set; }
+        public int Subjectid { get; set; }
+        public int Studentid { get; set; }
+        
+    }
+    public class subject
+    {
+        public int Subjectid { get; set; }
+        public string Subname { get; set; }
+        public string Class { get; set; }
+    }
+    public class student
+    { 
+        public int Studentid { get; set; }
+        public string studname { get; set; }
+        public string Class { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public bool Enrollstudent(Marks m)
+        {
+            if (m.marks > 50&&m.Studentid>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Enrollstudent1()
+        {
+            if (studname != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
---
+++ SchoolCL/SchoolCLService/SchoolClassService.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SchoolCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCLService
+{
+    [TestFixture]
+    public class SchoolClassService
+    {
+        [TestCase(55,4, ExpectedResult = true)]
+       [TestCase(30,-1, ExpectedResult = false)]
+        public bool check1(int t,int j)
+        {
+            Marks m = new Marks();
+            student s1 = new student(); 
+            m.marks = t;
+            m.Studentid = j;
+            bool ans = s1.Enrollstudent( m);
+            return ans;
+        }
+        [TestCase("nandhu" ,ExpectedResult = true)]
+        [TestCase("", ExpectedResult = true)]
+        public bool check2(string h)
+        {
+            student s1=new student();
+            s1.studname = h;
+            s1.Enrollstudent1();
+            return true;
+        }
+    }
+}
